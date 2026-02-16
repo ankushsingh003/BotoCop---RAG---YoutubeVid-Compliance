d23dcc995238df837f2be25674f488d1f55b00e4
ankushsingh003 <rathorankushrana143@gmail.com>
2026-02-16 07:05:02 +0530

    Implement LangGraph workflow: Define state graph with index_video and auto_content nodes

---
+++ backend/src/graph/workflow.py
@@ -0,0 +1,27 @@
+from langgraph.graph import StateGraph , END
+
+from src.graph.state import VideoAuditState
+from src.graph.nodes import index_video_node , auto_content_node
+
+
+
+def create_graph():
+    # define the graph
+    graph_builder = StateGraph(VideoAuditState)
+
+    # add nodes
+    graph_builder.add_node("index_video" , index_video_node)
+    graph_builder.add_node("auto_content" , auto_content_node)
+
+    # add edges
+    graph_builder.add_edge("index_video" , "auto_content")
+    graph_builder.add_edge("auto_content" , END)
+
+    # compile the graph
+    video_audit_graph = graph_builder.compile()
+
+    return video_audit_graph
+
+
+# expose the runable video_audit_graph 
+video_audit_graph = create_graph()
